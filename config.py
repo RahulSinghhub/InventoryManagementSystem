0284ecc23d61732a4501c2d5caf8515cf1921bdf
laraib07 <a.laraib@protonmail.com>
2024-10-02 23:47:30 +0530

    add env file

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',
-        database='exl_project',
-        user='root',
-        password='1234'
+        host=os.environ.get('DB_HOST'),
+        database=os.environ.get('DB_DATABASE'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD')
     )
 
-EMAIL_USER = "your_email@gmail.com"
-EMAIL_PASSWORD = "your_email_password"
-RECIPIENT_EMAIL = "store_manager_email@example.com"
+EMAIL_USER = os.environ.get('MAIL_USER')
+EMAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+RECIPIENT_EMAIL = os.environ.get('RECIPIENT_EMAIL')
